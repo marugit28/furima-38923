821386a2af5690bd597e685013f7982e1b988289
marugit28 <Yaname2833@icloud.com>
2023-03-06 14:15:03 +0900

    ユーザー修正1

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,12 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :email,    presence: true
   validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-  validates :last_name, presence: true, format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/ }
-  validates :first_name, presence: true, format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :last_name_kana, presence: true, format: { with: /\A([ァ-ン]|ー)+\z/ }
   validates :first_name_kana, presence: true, format: { with: /\A([ァ-ン]|ー)+\z/ }
   validates :birthday, presence: true
 
-  has_many :items
-  has_many :orders
+  # has_many :items
+  # has_many :orders
 end
