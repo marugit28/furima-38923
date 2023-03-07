f0349001751479351e6731b18b3db84d558b75fd
marugit28 <Yaname2833@icloud.com>
2023-03-07 13:04:45 +0900

    Active Strageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,9 @@
 class ItemsController < ApplicationController
   def index
+    @item = Item.new
+    @user = User.find(params[:user])
   end
+
+  def message_params
+    params.require(:item).permit(:content,:image).merge(user_id: current_user.id)
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+  create_table :items do |t|
+    validates :item_name,                       precense: true
+    validates :explanation,                     precense: true
+    validates :category_id,                     precense: true
+    validates :state_id,                        precense: true
+    validates :region_id,                       precense: true
+    validates :scheduled_delivery_id,           precense: true
+    validates :shopping_fee_id,                 precense: true
+    validates :user,                            precense: true, foreign_key: true
+    validates :price,                           precense: true
+
+    has_one :order
+    belongs_to :user
+    has_one_attached :image
+  end
 end
