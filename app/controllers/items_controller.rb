c1d1f0b77482c81a9fe45b0c32fab291ba63f573
marugit28 <Yaname2833@icloud.com>
2023-03-13 16:29:45 +0900

    情報編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!,only: [:new, :edit]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -23,10 +24,26 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
-    params.permit(:image, :item_name, :explanation, :category_id, :state_id, :region_id, :scheduled_delivery_id,
+    params.require(:item).permit(:image, :item_name, :explanation, :category_id, :state_id, :region_id, :scheduled_delivery_id,
                   :shopping_fee_id, :price).merge(user_id: current_user.id)
   end
 end
